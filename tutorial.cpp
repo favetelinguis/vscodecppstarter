98526b43b907af016a7bd095bf7b55490b15e770
Henrik Larsson <reg@henkelarsson.se>
2018-01-15 12:53:02 +0100

    Updated c++ flags for c++11

--- tutorial.cpp
+++ tutorial.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <map>
 
 int main(int argc, const char * argv[]) {
     int a;
-    std::cout << "Hello world";
+    std::map<char, int> items;
+    items['a'] = 1;
+    items['b'] = 2;
+    for (auto i : items) {
+        std::cout << i.first;
+    }
     std::cin >> a;
     return 0;
 }
